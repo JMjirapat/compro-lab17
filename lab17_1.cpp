ca8f3ff0aa8c7afda5cf8e81c216265b14ab5a98
JMjirapat <jm_jirapat@hotmail.com>
2022-01-17 21:41:43 +0700

    Lab17 - All Complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void randData(double *dPtr,int N,int M){
+	for(int i=0; i<N*M; i++){
+		*(dPtr+i) = rand()%101/100.0;
+	}
+}
+
+void showData(double *dPtr,int N,int M){
+	cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i);
+        if(i%M==M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void findRowSum(const double *dPtr,double *sumRow,int N,int M){
+	for(int i = 0; i < N; i++){
+		sumRow[i] = 0;
+		for(int j = 0; j < M; j++){
+			sumRow[i] += *(dPtr+j+M*i);
+		}
+    }
+}
+
+void findColSum(const double *dPtr,double *sumCal,int N,int M){
+	for(int j = 0; j < M; j++){
+			sumCal[j] = 0;
+	}
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < M; j++){
+			sumCal[j] += *(dPtr+j+M*i);
+		}
+    }
+}
